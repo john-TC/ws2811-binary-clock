fde0b7953d3d5a31754fc656c22f75ad3076b3d7
John <pattersonj@pm.me>
2020-02-27 02:20:25 -0700

    Add outside temperature from OpenWeatherMap API

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <ESP8266WiFi.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 
 #define LED_TYPE WS2811
 #define NUM_LEDS 50
@@ -22,6 +24,8 @@ int ledPinsDate[] = {18, 17, 16, 15, 14};
 int ledPinsMonth[] = {33, 34, 35, 36};
 int ledPinsYear[] = {42, 41, 40, 39, 38};
 int ledPinsDay[] = {19, 32, 43};
+int ledPinsTempAbove[] = {8, 9, 10, 11, 12, 13};
+int ledPinsTempBelow[] = {7, 6, 5, 4, 3, 2};
 
 CHSV orange = CHSV(17, 255, 255);
 CHSV white = CHSV(0, 0, 235);
@@ -32,18 +36,16 @@ CHSV darkBlue = CHSV(148, 255, 153);
 int prevTime = -1;
 int currentTime = -1;
 int prevMins = -1;
-int currentMins = -1;
 int prevHours = -1;
-int currentHours = -1;
 int prevDate = -1;
-int currentDate = -1;
 int prevMonth = -1;
-int currentMonth = -1;
 int prevYear = -1;
-int currentYear = -1;
+int prevTemp;
 bool updated = false;
 
 WiFiUDP ntpUDP;
+WiFiClient client;
+HTTPClient http;
 NTPClient timeClient(ntpUDP, -25200);
 CRGB leds[NUM_LEDS];
 
@@ -54,6 +56,8 @@ CRGB leds[NUM_LEDS];
 #define nBitsMonth sizeof(ledPinsMonth)/sizeof(ledPinsMonth[0])
 #define nBitsYear sizeof(ledPinsYear)/sizeof(ledPinsYear[0])
 #define nBitsDay sizeof(ledPinsDay)/sizeof(ledPinsDay[0])
+#define nBitsTempAbove sizeof(ledPinsTempAbove)/sizeof(ledPinsTempAbove[0])
+#define nBitsTempBelow sizeof(ledPinsTempBelow)/sizeof(ledPinsTempBelow[0])
 
 void dispBinarySec(byte nSec) {
   for (byte i = 0; i < nBitsSec; i++) {
@@ -70,9 +74,8 @@ void dispBinarySec(byte nSec) {
 }
 
 void dispBinaryMin(byte nMin) {
-  currentMins = timeClient.getMinutes();
-  if (currentMins != prevMins) {
-    prevMins = currentMins;
+  if (nMin != prevMins) {
+    prevMins = nMin;
     for (byte i = 0; i < nBitsMin; i++) {
       if (nMin & 1) {
         leds[ledPinsMin[i]] = lightBlue;
@@ -88,9 +91,8 @@ void dispBinaryMin(byte nMin) {
 }
 
 void dispBinaryHr(byte nHr) {
-  currentHours = timeClient.getHours();
-  if (currentHours != prevHours) {
-    prevHours = currentHours;
+  if (nHr != prevHours) {
+    prevHours = nHr;
     for (byte i = 0; i < nBitsHr; i++) {
       if (nHr & 1) {
         leds[ledPinsHr[i]] = darkBlue;
@@ -123,8 +125,8 @@ void dispBinaryDay(byte nDay) {
 }
 
 void dispBinaryDate(byte nDate) {
-  currentDate = timeClient.getDate();
-  if (currentDate != prevDate) {
+  if (nDate != prevDate) {
+    prevDate = nDate;
     dispBinaryDay(timeClient.getDay());
     for (byte i = 0; i < nBitsDate; i++) {
       if (nDate & 1) {
@@ -141,8 +143,8 @@ void dispBinaryDate(byte nDate) {
 }
 
 void dispBinaryMonth(byte nMonth) {
-  currentMonth = timeClient.getMonth();
-  if (currentMonth != prevMonth) {
+  if (nMonth != prevMonth) {
+    prevMonth = nMonth;
     for (byte i = 0; i < nBitsMonth; i++) {
       if (nMonth & 1) {
         leds[ledPinsMonth[i]] = lightBlue;
@@ -158,8 +160,8 @@ void dispBinaryMonth(byte nMonth) {
 }
 
 void dispBinaryYear(byte nYear) {
-  currentYear = timeClient.getYear();
-  if (currentYear != prevYear) {
+  if (nYear != prevYear) {
+    prevYear = nYear;
     for (byte i = 0; i < nBitsYear; i++) {
       if (nYear & 1) {
         leds[ledPinsYear[i]] = darkBlue;
@@ -174,6 +176,60 @@ void dispBinaryYear(byte nYear) {
   }
 }
 
+int getTemp() {
+  http.begin("http://api.openweathermap.org/data/2.5/weather?id={CITY-ID}&appid={API-KEY}&units=metric");
+  http.GET();
+  const size_t capacity = JSON_ARRAY_SIZE(3) + 2*JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(2) + 3*JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(6) + JSON_OBJECT_SIZE(12) + 480;
+  DynamicJsonBuffer jsonBuffer(capacity);
+  String json = http.getString();
+  JsonObject& root = jsonBuffer.parseObject(json);
+  JsonObject& main = root["main"];
+  float main_temp = main["temp"];
+  return round(main_temp);
+}
+
+void dispBinaryTemp(int nTemp) {
+  if (nTemp != prevTemp) {
+    if (nTemp >= 0) {
+      prevTemp = nTemp;
+      for (byte i = 0; i < nBitsTempBelow; i++) {
+        leds[ledPinsTempBelow[i]] = CHSV(0, 0, 0);
+        FastLED.show();
+      }
+      for (byte i = 0; i < nBitsTempAbove; i++) {
+        if (nTemp & 1) {
+          leds[ledPinsTempAbove[i]] = CHSV(0, 255, 255);
+          FastLED.show();
+        }
+        else {
+          leds[ledPinsTempAbove[i]] = CHSV(0, 0, 0);
+          FastLED.show();
+        }
+        nTemp /= 2;
+      }
+    }
+    else {
+      nTemp = nTemp * -1;
+      prevTemp = nTemp;
+      for (byte i = 0; i < nBitsTempAbove; i++) {
+        leds[ledPinsTempAbove[i]] = CHSV(0, 0, 0);
+        FastLED.show();
+      }
+      for (byte i = 0; i < nBitsTempBelow; i++) {
+        if (nTemp & 1) {
+          leds[ledPinsTempBelow[i]] = CHSV(160, 255, 255);
+          FastLED.show();
+        }
+        else {
+          leds[ledPinsTempBelow[i]] = CHSV(0, 0, 0);
+          FastLED.show();
+        }
+        nTemp /= 2;
+      }
+    }
+  }
+}
+
 void setupWifi() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -183,6 +239,7 @@ void setupWifi() {
   }
   digitalWrite(WIFI_LED, HIGH);
   timeClient.update();
+  dispBinaryTemp(getTemp());
 }
 
 void setup() {
@@ -208,22 +265,16 @@ void loop() {
     dispBinaryDate(timeClient.getDate());
     dispBinaryMonth(timeClient.getMonth());
     dispBinaryYear(timeClient.getYear() % 100);
-    // Serial.println(timeClient.getFormattedTime());
     prevTime = currentTime;
     if (updated) {
       updated = false;
     }
   }
+  if (currentTime % 900 == 0) {
+    dispBinaryTemp(getTemp());
+  }
   if (currentTime % 3600 == 0 && !updated) {
-    // int oldTime = timeClient.getEpochTime();
     timeClient.forceUpdate();
-    // int newTime = timeClient.getEpochTime();
-    // Serial.print("Old time: ");
-    // Serial.println(oldTime);
-    // Serial.print("New time: ");
-    // Serial.println(newTime);
-    // Serial.print("Difference: ");
-    // Serial.println(newTime - oldTime);
     updated = true;
   }
 }
